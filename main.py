a3e92db06b29b4b7c968af3f2b7921e74daba026
Sanjai P <sanjaiprabhakaran.official@gmail.com>
2023-10-26 17:07:09 +0530

    added model

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 app = FastAPI()
+from transformers import pipeline
 
 @app.get("/")
 def hello():
-    return {"hello": "You Successfully deployed your first api"}
+    classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True)
+    prediction = classifier("I love using transformers. The best part is wide range of support and its easy to use", )
+    return prediction
+    # print(prediction)
+    # return {"hello": "You Successfully deployed your first api"}
 
